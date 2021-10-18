7a9e9700f2db936b76dfd8cafc3ee65a7e33a96d
Nuttawut503 <nuttawut.promsuk@gmail.com>
2021-10-18 21:04:12 +0700

    added program/PLO to create and read

--- graph/schema.program.resolvers.go
+++ graph/schema.program.resolvers.go
@@ -5,27 +5,104 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/Nuttawut503/capstone-backend/db"
 	"github.com/Nuttawut503/capstone-backend/graph/model"
+	"github.com/prisma/prisma-client-go/runtime/transaction"
 )
 
 func (r *mutationResolver) CreateProgram(ctx context.Context, input model.CreateProgramInput) (*model.Program, error) {
-	panic(fmt.Errorf("not implemented"))
+	createdProgram, err := r.Client.Program.CreateOne(
+		db.Program.Name.Set(input.Name),
+		db.Program.Description.Set(input.Description),
+	).Exec(ctx)
+	if err != nil {
+		return &model.Program{}, err
+	}
+	return &model.Program{
+		ID:          createdProgram.ID,
+		Name:        createdProgram.Name,
+		Description: createdProgram.Description,
+	}, nil
 }
 
 func (r *mutationResolver) CreatePLOGroupInput(ctx context.Context, input model.CreatePLOGroupInput) (*model.PLOGroup, error) {
-	panic(fmt.Errorf("not implemented"))
+	createPLOGroup, err := r.Client.PLOgroup.CreateOne(
+		db.PLOgroup.Name.Set(input.Name),
+		db.PLOgroup.Program.Link(
+			db.Program.ID.Equals(input.ProgramID),
+		),
+	).Exec(ctx)
+	if err != nil {
+		return &model.PLOGroup{}, err
+	}
+	transactions := []transaction.Param{}
+	for _, plo := range input.Plos {
+		transactions = append(transactions, r.Client.PLO.CreateOne(
+			db.PLO.Title.Set(plo.Title),
+			db.PLO.Description.Set(plo.Description),
+			db.PLO.PloGroup.Link(
+				db.PLOgroup.ID.Equals(createPLOGroup.ID),
+			),
+		).Tx())
+	}
+	if err := r.Client.Prisma.Transaction(transactions...).Exec(ctx); err != nil {
+		return &model.PLOGroup{}, err
+	}
+	return &model.PLOGroup{
+		ID:   createPLOGroup.ID,
+		Name: createPLOGroup.Name,
+	}, nil
 }
 
 func (r *queryResolver) Programs(ctx context.Context) ([]*model.Program, error) {
-	panic(fmt.Errorf("not implemented"))
+	allPrograms, err := r.Client.Program.FindMany().Exec(ctx)
+	if err != nil {
+		return []*model.Program{}, err
+	}
+	programs := []*model.Program{}
+	for _, program := range allPrograms {
+		programs = append(programs, &model.Program{
+			ID:          program.ID,
+			Name:        program.Name,
+			Description: program.Description,
+		})
+	}
+	return programs, nil
+
 }
 
 func (r *queryResolver) PloGroups(ctx context.Context, programID string) ([]*model.PLOGroup, error) {
-	panic(fmt.Errorf("not implemented"))
+	allPLOGroups, err := r.Client.PLOgroup.FindMany(
+		db.PLOgroup.ProgramID.Equals(programID),
+	).Exec(ctx)
+	if err != nil {
+		return []*model.PLOGroup{}, err
+	}
+	ploGroups := []*model.PLOGroup{}
+	for _, ploGroup := range allPLOGroups {
+		ploGroups = append(ploGroups, &model.PLOGroup{
+			ID:   ploGroup.ID,
+			Name: ploGroup.Name,
+		})
+	}
+	return ploGroups, nil
 }
 
 func (r *queryResolver) Plos(ctx context.Context, ploGroupID string) ([]*model.Plo, error) {
-	panic(fmt.Errorf("not implemented"))
+	allPLOs, err := r.Client.PLO.FindMany(
+		db.PLO.PloGroupID.Equals(ploGroupID),
+	).Exec(ctx)
+	if err != nil {
+		return []*model.Plo{}, err
+	}
+	plos := []*model.Plo{}
+	for _, plo := range allPLOs {
+		plos = append(plos, &model.Plo{
+			ID:          plo.ID,
+			Title:       plo.Title,
+			Description: plo.Description,
+		})
+	}
+	return plos, nil
 }
